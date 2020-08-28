3997d9c7098ff54fd6fe71420d6f7cb4df63fe6b
Mark Houghton <markhoughton@Marks-MacBook-Pro.local>
2020-08-28 10:21:35 +0100

    added comments

--- connect4.js
+++ connect4.js
@@ -15,20 +15,23 @@ function resetGrid() {
 }
 
 function createGrid(numRows, numColumns) {
-    // const gridDiv = document.createElement("div");
-    // gridDiv.id = "grid";
-    // document.body.appendChild(gridDiv);
+
+    // set up divs for storing each row of elements
     for (let i = 0; i < numRows; i++) {
         const rowDiv = document.createElement("div");
         rowDiv.className = "row";
         rowDiv.id = "row-"+i;
         rowDiv.style.width = "" + (numColumns*100) + "px"
         document.getElementById("grid").appendChild(rowDiv);
+
+        // set up divs for individual elements
         for (let j = 0; j < numColumns; j++) {
             const colDiv = document.createElement("div");
             colDiv.className = "column";
             colDiv.id = "row-"+i+"-column-"+j;
             document.getElementById("row-"+i).appendChild(colDiv);
+
+            // listen for clicks 
             colDiv.addEventListener("click", function () {
                 colDiv.style.backgroundColor = (player % 2 == 0 ? "red" : "yellow");
                 player++;
