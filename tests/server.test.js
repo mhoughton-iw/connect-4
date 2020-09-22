7ce9a434225f6e50e5d66b9c42e49d53d7a40761
mhoughton-iw <mark.houghton@infinityworks.com>
2020-09-22 16:05:31 +0100

    began trying to mock game method

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises;
 const request = require('supertest');
+const each = require('jest-each').default;
 const { Game } = require('../server/game');
 const winner = require('../server/winner');
 const { app } = require('../server/server');
@@ -28,6 +29,56 @@ describe('GET /game/state', () => {
     request(app)
       .get('/game/state')
       .expect(200, JSON.stringify(game))
+      .end(done);
+  });
+});
+
+describe('POST /game/board/col/:c', () => {
+  beforeAll(() => {
+    jest.spyOn(Game.prototype, 'takeTurn').mockImplementation((col) => {
+      this.state = [
+        [null, null, 'hello', null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ];
+      this.turn = 30;
+    });
+  });
+
+  // each([
+  //   // [0], [1], [2],
+  //   0, 1, 2,
+  // ])
+  test.skip('should return the game state after placing counter', (col, done) => {
+    // const col = 2;
+    const game = {
+      numRows: 6,
+      numCols: 7,
+      turn: 0,
+      state:
+      [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ],
+    };
+    game.state[0][col] = 0;
+    console.log(game.state);
+    console.log(`just printed expected for col ${col}`);
+    request(app)
+      .post(`/game/board/col/${col}`)
+      // .expect((res) => {
+      //   // console.log(res);
+      // })
+      // .send(game)
+      .expect(200)
+      .expect(JSON.stringify(game))
       .end(done);
   });
 });
